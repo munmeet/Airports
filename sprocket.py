bb0107c1c43f216c1a53756c31dd2a5b7641ce73
manu <munmeetkailley19@gmail.com>
2025-01-19 13:19:45 -0800

    SAN-NAS

--- sprocket.py
+++ sprocket.py
@@ -1,23 +1,41 @@
 #!/usr/bin/env python3
+import time
 import json
 
-with open('airports.json', 'r') as openfile:
-    
+with open("airports.json", "r") as openfile:
     y = json.load(openfile)
-
 x = y.keys()
 
-#class airport: 
-#    index = "rcmp"
-#    timestamp = "2025-01-16 15:20:37.855291"
+gate = {"icao" : "S", "iata" : "S", "name" : "S", "city" : "S", "state" : "S", "country" : "S", "elevation" : "N", "lat" :  "N", "lon" : "N", "tz" : "S"}
 
-def targeter(airport):
+def targeter(airport,taxi):
    z = y[airport]
-   print(z,"\n")
+   g = str(z.get(taxi)) #10 prints
+   key1 = f"\"{taxi}\":"
+   key2 = gate.get(taxi)
+   key3 = g
+   k = {key2:key3}
+   m = key1 + str(k)
+   return m
+###################################
 
-def targets():    
-    for index in x: 
-        target = index
-        #print(target, "[OK]")
-        targeter(target)
+def targets():
+    for target in x:
+        folder= "tmp/"
+        file = target + ".json"
+        filepath = folder + file
+        with open(filepath, "w") as disk:
+            disk.write("{\n")
+            disk.close()
+            for targeted in gate:
+                terminal = targeter(target,targeted)
+                with open(filepath, "a") as disk:
+                    disk.write(str(terminal))
+                    disk.write(",\n")
+                print("\n",target,"[OK]")
+            with open(filepath, 'a') as disk:
+                date = time.time()
+                timestamp =  f"\"timestamp\":{{\"S\":\"{date}\"}}"
+                disk.write(timestamp)
+                disk.write("\n}")
 targets()
